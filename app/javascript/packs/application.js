f41d3893749cc097115731da019691f33aaeeed7
tonsojun <josephjun@gmail.com>
2021-05-16 09:05:55 -0700

    FullCalendar working, code currently in application.js, will move code out into separate files to better organize

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,80 +17,24 @@ import { Calendar } from "@fullcalendar/core"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import listPlugin from "@fullcalendar/list"
-import flatpickr from "flatpickr"
-
-require("flatpickr/dist/themes/dark.css");
-
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-//document.addEventListener("turbolinks:load", () => {
- 
-//   $(function () {
-//     var calendarEl = document.getElementById("calendar");
-//     var calendar = new Calendar(calendarEl, {
-//       header: {
-//         left: "prev,next",
-//         right: "dayGridMonth, listMonth",
-//       },
-
-//       plugins: [dayGridPlugin, listPlugin],
-//       defaultView: "dayGridMonth",
-//     });
-
-//     calendar.render();
-  
-//   });
-
 document.addEventListener("turbolinks:load", () => {
 
-var initialize_calendar;
-initialize_calendar = function() {
-  console.log("hello world")
-  $('#calendar').each(function(){
-    var calendar = $(this);
-    calendar.fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
-      },
-      selectable: true,
-      selectHelper: true,
-      editable: true,
-      eventLimit: true,
-      events: '/events.json',
-
-      select: function(start, end) {
-        $.getScript('/events/new', function() {});
-
-        calendar.fullCalendar('unselect');
-      },
-
-      eventDrop: function(event, delta, revertFunc) {
-        event_data = { 
-          event: {
-            id: event.id,
-            start: event.start.format(),
-            end: event.end.format()
-          }
-        };
-        $.ajax({
-            url: event.update_url,
-            data: event_data,
-            type: 'PATCH'
-        });
-      },
-      
-      eventClick: function(event, jsEvent, view) {
-        $.getScript(event.edit_url, function() {});
-      }
+  $(function () {
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new Calendar(calendarEl, {
+      headerToolbar: { center: 'dayGridMonth,timeGridWeek,timeGridDay' },
+      plugins: [dayGridPlugin, listPlugin, timeGridPlugin],
+      defaultView: "dayGridMonth",
+      initalView: 'timeGridWeek'
     });
-  })
-};
-initialize_calendar
-
+    console.log("are you reaching me?");
+    
+    calendar.render();
+  
+  });
 })
-// $(document).on('turbolinks:load', initialize_calendar);
